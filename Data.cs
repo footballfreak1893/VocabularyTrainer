30dc6bbd1f6b588733e447f54fb152ab541a2e99
footballfreak1893 <schwabenfreak@googlemail.com>
2019-10-27 15:36:56 +0100

    restructured class

--- Data.cs
+++ Data.cs
@@ -12,59 +12,12 @@ namespace VocabularyApp
         
         //paths for text/excel files
         public static string pathMainFolder = @"Data";
-        //public static string pathAllWordsTextFile = @"Data/AllWordsList.txt";
         public static string pathOutputExcel = @"Data/ExcelList.xlsx";
-        //public static string pathFailureWordsTextFile = @"Data/FailureWordsList.txt";
-        //public static string pathSuccessWordsTextFile = @"Data/SuccessWordsList.txt";
         public static string pathAllWords = @"Data/AllWordsList";
         public static string pathAllWordsID = @"Data/AllWordsIDList";
         public static string pathFailureWords = @"Data/FailureWordsList";
-        public static string pathSuccessWords = @"Data/SuccessWordsList";
 
-
-        //Old Text
-        public static string pathFailureWordsTextFile = @"Data/FailureWordsList.txt";
-        public static string pathSuccessWordsTextFile = @"Data/SuccessWordsList.txt";
         public static string pathAllWordsTextFile = @"Data/AllWordsList.txt";
-
-
-
-
-
-        public static void CreateOrLoadMainData()
-        {
-            if (!Directory.Exists(pathMainFolder))
-            {
-
-                Directory.CreateDirectory(pathMainFolder);
-            }
-
-
-            
-            if (!File.Exists(pathAllWords))
-            {
-                //neue Voc erstellen
-                File.Create(pathAllWords);
-            }
-
-
-            if (!File.Exists(pathFailureWords))
-            {
-                File.Create(pathFailureWords);
-
-            }
-
-            if (!File.Exists(pathSuccessWords))
-            {
-                File.Create(pathSuccessWords);
-            }
-
-            //if (!File.Exists(pathAllWordsID))
-            //{
-            //    File.Create(pathAllWordsID);
-            //}
-
-        }
 
 
     }
